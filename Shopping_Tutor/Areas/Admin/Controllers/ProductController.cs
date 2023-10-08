6569f710ab78c1b4dcce3df435c73726d8d9d071
Nam Hoai <hoainamcs2002@gmail.com>
2023-10-08 15:58:33 +0700

    Admin SP

---
+++ Shopping_Tutor/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_Tutor.Repository;
+
+namespace Shopping_Tutor.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
