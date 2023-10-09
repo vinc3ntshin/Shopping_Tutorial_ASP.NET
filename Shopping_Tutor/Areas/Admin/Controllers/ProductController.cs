49e3b902112a4be1f6ccdf9668e99903989d7cf8
Nam Hoai <hoainamcs2002@gmail.com>
2023-10-09 23:33:25 +0700

    Xac Thuc File Upload

--- Shopping_Tutor/Areas/Admin/Controllers/ProductController.cs
+++ Shopping_Tutor/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Shopping_Tutor.Models;
 using Shopping_Tutor.Repository;
 
 namespace Shopping_Tutor.Areas.Admin.Controllers
@@ -17,11 +18,18 @@ namespace Shopping_Tutor.Areas.Admin.Controllers
         {
             return View(await _dataContext.Products.OrderByDescending(p => p.Id).Include(p => p.Category).Include(p => p.Brand).ToListAsync());
         }
+        [HttpGet]
         public IActionResult Create()
         {
             ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name");
             ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name");
             return View();
+        }
+        public async Task<IActionResult> Create(ProductModel product)
+        {
+            ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
+            ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
+            return View(product);
         }
     }
 }
