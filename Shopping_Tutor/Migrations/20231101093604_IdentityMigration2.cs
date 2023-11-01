b196a03f4537221fd1f3d89cb07cd7607d1d6ecd
Nam Hoai <hoainamcs2002@gmail.com>
2023-11-01 16:39:03 +0700

    Identity

---
+++ Shopping_Tutor/Migrations/20231101093604_IdentityMigration2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping_Tutor.Migrations
+{
+    public partial class IdentityMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Occupation",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Occupation",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Shopping_Tutor/Repository/DataContext.cs
+++ Shopping_Tutor/Repository/DataContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Shopping_Tutor.Models;
 
 namespace Shopping_Tutor.Repository
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUserModel>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) 
         { 
