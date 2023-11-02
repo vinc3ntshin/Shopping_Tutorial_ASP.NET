91d6f9f14d13c607697f418f3fb474cdd6d2fc9d
Nam Hoai <hoainamcs2002@gmail.com>
2023-11-02 16:24:02 +0700

    Create User

--- Shopping_Tutor/Controllers/AccountController.cs
+++ Shopping_Tutor/Controllers/AccountController.cs
@@ -1,26 +1,52 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Shopping_Tutor.Models;
+using Shopping_Tutor.Repository;
 
 namespace Shopping_Tutor.Controllers
 {
-    public class AccountController : Controller
-    {
-        private UserManager<AppUserModel> _userManager;
-        private SignInManager<AppUserModel> _signInManager;
-        public AccountController(SignInManager<AppUserModel> signInManager, UserManager<AppUserModel> userManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
-         
-        public async Task<IActionResult> Login()
-        {
-            return View();
-        }
-    }
+	public class AccountController : Controller
+	{
+		private UserManager<AppUserModel> _userManager;
+		private SignInManager<AppUserModel> _signInManager;
+		public AccountController(SignInManager<AppUserModel> signInManager, UserManager<AppUserModel> userManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> Login()
+		{
+			return View();
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(UserModel user)
+		{
+			if (ModelState.IsValid)
+			{
+				AppUserModel newUser = new AppUserModel { UserName = user.UserName, Email = user.Email };
+				IdentityResult result = await _userManager.CreateAsync(newUser);
+				if (result.Succeeded)
+				{
+					TempData["Success"] = "Tạo user thành công";
+					return Redirect("/Account");
+				}
+				foreach(IdentityError error in result.Errors)
+				{
+					ModelState.AddModelError("",error.Description);
+				}
+			}
+			return View(user);
+		}
+	}
 }
